e24c3198d0b83f8e2c300284217b1718fd92a97a
Manrriquez <manrriquez.contato@gmail.com>
2023-02-03 18:54:42 -0300

    feat: remove data send email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,3 +1,3 @@
-export const BASE_SEND_SERVICE = "service_6zkf28d";
-export const BASE_SEND_TEMPLATE = "template_bdxgdqj";
-export const BASE_SEND_KEY = "j8bW15sy9vE5yMDmW";
+export const BASE_SEND_SERVICE = "";
+export const BASE_SEND_TEMPLATE = "";
+export const BASE_SEND_KEY = "";
